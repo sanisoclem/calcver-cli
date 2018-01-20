d8208ebce99045f73d1615f01291d6bc1ad5bf93
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-01-20 19:39:42 +0800

    chore: travis setup, cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate clap;
 extern crate calcver;
 extern crate git2;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg};
 use calcver::config::{ProjectConfig};
 use calcver::{VersionBumpBehavior};
 
@@ -40,7 +40,7 @@ fn main() {
         _ => repogit::GitRepo::from(".")
     };
 
-    let configPath = match matches.value_of("config") {
+    let config_path = match matches.value_of("config") {
         Some(path)=> path,
         _=> "calcver.yml"
     };
@@ -57,31 +57,4 @@ fn main() {
     }
 
     println!("Next version is: {}",version);
-    
-
-
-    // if let Some(c) = matches.value_of("config") {
-    //     println!("Value for config: {}", c);
-    // }
-
-    // // You can see how many times a particular flag or argument occurred
-    // // Note, only flags can have multiple occurrences
-    // match matches.occurrences_of("debug") {
-    //     0 => println!("Debug mode is off"),
-    //     1 => println!("Debug mode is kind of on"),
-    //     2 => println!("Debug mode is on"),
-    //     3 | _ => println!("Don't be crazy"),
-    //  }
-
-    // // You can check for the existence of subcommands, and if found use their
-    // // matches just as you would the top level app
-    //  if let Some(ref matches) = matches.subcommand_matches("test") {
-    //      // "$ myapp test" was run
-    //      if matches.is_present("list") {
-    //          // "$ myapp test -l" was run
-    //          println!("Printing testing lists...");
-    //      } else {
-    //          println!("Not printing testing lists...");
-    //      }
-    //  }
 }
