4e3c3311cfa2179aefb4dc813efc4816125f3a46
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-01-20 19:16:01 +0800

    feat: git integration

---
+++ src/repogit.rs
@@ -0,0 +1,65 @@
+use calcver;
+use git2::{Repository};
+use std::collections::HashMap;
+
+pub struct GitRepo {
+    last_tag: Option<String>,
+    commits_since_last_tag: Vec<String>
+}
+
+impl calcver::repository::Repository for GitRepo {
+    fn get_last_tag(&self) -> Option<&str> {
+        match &self.last_tag {
+            &Some(ref tag) => Some(tag),
+            &None=>None
+        }
+    }
+    fn get_commits_since_last_tag(&self) -> &Vec<String> {
+        &self.commits_since_last_tag
+    }
+}
+
+impl GitRepo {
+    pub fn from(path: &str) -> GitRepo {
+        let r = Repository::open(&path).unwrap();
+        let tags = r.tag_names(Some("*")).unwrap();
+        
+        let tag_map: HashMap<_,_> = tags.iter().filter_map(|t| {
+        match t {
+            Some(name) => {
+                if let Ok(obj) = r.revparse_single(name) {
+                    if let Some(tag) = obj.as_tag() {
+                        if let Ok(commit) = tag.peel() {
+                            Some((commit.id(),String::from(name)))
+                        } else {None}
+                    } else if let Some(commit) = obj.as_commit() {
+                        Some((commit.id(),String::from(name)))
+                    } else { None }
+                } else { None }
+            },
+            _ => None
+        }
+    }).collect();
+
+        let mut revwalk = r.revwalk().unwrap();
+        let mut tag:Option<String> = None;
+        let mut commits:Vec<String> = vec![];
+        revwalk.push_head().unwrap();
+
+        for c in revwalk {
+            let commit =r.find_commit(c.unwrap()).unwrap();
+            if let Some(tg) = tag_map.get(&commit.id()) {
+                tag = Some(tg.to_owned());
+                break;
+            }
+            if let Some(msg) = commit.message() {
+                commits.push(msg.to_string());
+            }
+        }
+
+        GitRepo {
+            last_tag: tag,
+            commits_since_last_tag: commits
+        }
+    }
+}
